33328d67391510b6d955d0f28b6af51522597707
cmottao <cristianstevenmottaojeda@gmail.com>
2023-04-16 16:18:27 -0500

    added problems

---
+++ cses/introductory/palindromeReorder.cpp
@@ -0,0 +1,56 @@
+// Made by Cristian Motta
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define double long double
+
+const int MAX = 1e6 + 10;
+const int MIN = -MAX;
+
+void solve() {
+    string st; cin >> st;
+    map<char, int> counts;
+    int odds = 0;
+
+    for(char ch : st) {
+        counts[ch]++;
+    }
+
+    string left = "", right, middle = "";
+    bool s = true;
+
+    for(auto pair : counts) {
+        if(pair.second % 2 != 0) {
+            if(odds == 0) {
+                middle += string(pair.second, pair.first);
+                odds++;
+            }
+            else {
+                s = false;
+            }
+        }
+        else {
+            left += string(pair.second / 2, pair.first);
+        }
+    }
+    right = left; reverse(right.begin(), right.end());
+    string palindrome = left + middle + right;
+
+    if(s) {
+        cout << palindrome;
+    }
+    else {
+        cout << "NO SOLUTION";
+    }
+}
+
+int32_t main() {
+    int tc = 1;
+    // cin >> tc;
+
+    for(int t = 1; t <= tc; t++) {
+        solve();
+    }
+    return 0;
+}
---
+++ cses/sorting and searching/distinctNumbers.cpp
@@ -0,0 +1,30 @@
+// Made by Cristian Motta
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define double long double
+
+const int MAX = 1e6 + 10;
+const int MIN = -MAX;
+
+void solve() {
+    int n; cin >> n;
+    map<int, int> numbers;
+
+    for(int i = 0; i < n; i++) {
+        int x; cin >> x;
+        numbers[x]++;
+    }
+    cout << numbers.size();
+}
+
+int32_t main() {
+    int tc = 1;
+    // cin >> tc;
+
+    for(int t = 1; t <= tc; t++) {
+        solve();
+    }
+    return 0;
+}
