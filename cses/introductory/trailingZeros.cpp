beb371883af09c86f32fd10186286efcded7d0b6
cmottao <cristianstevenmottaojeda@gmail.com>
2023-04-20 00:22:24 -0500

    "added problems"

---
+++ cses/introductory/trailingZeros.cpp
@@ -0,0 +1,29 @@
+// Made by Cristian Motta
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define double long double
+
+const int MAX = 1e6 + 10;
+const int MIN = -MAX;
+
+void solve() {
+    int n; cin >> n;
+    int ans = 0;
+
+    for(int i = 5; i <= n; i *= 5) {
+        ans += n / i;
+    }
+    cout << ans;
+}
+
+int32_t main() {
+    int tc = 1;
+    // cin >> tc;
+
+    for(int t = 1; t <= tc; t++) {
+        solve();
+    }
+    return 0;
+}
---
+++ cses/range queries/staticRangeSumQueries.cpp
@@ -0,0 +1,34 @@
+// Made by Cristian Motta
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define double long double
+
+const int MAX = 1e6 + 10;
+const int MIN = -MAX;
+
+void solve() {
+    int n, q; cin >> n >> q;
+    int prefix_sum[n];
+
+    for(int i = 1; i <= n; i++) {
+        int a; cin >> a;
+        prefix_sum[i] = prefix_sum[i - 1] + a;
+    }
+
+    for(int i = 0; i < q; i++) {
+        int a, b; cin >> a >> b;
+        cout << prefix_sum[b] - prefix_sum[a - 1] << endl;
+    }
+}
+
+int32_t main() {
+    int tc = 1;
+    // cin >> tc;
+
+    for(int t = 1; t <= tc; t++) {
+        solve();
+    }
+    return 0;
+}
