badfbaf4230c0ae1792de6e9e3bb1292a4c8310f
cmottao <cristianstevenmottaojeda@gmail.com>
2023-07-22 23:35:20 -0500

    added algorithms

--- algorithms/binarySearch.cpp
+++ algorithms/binarySearch.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // Performs binary search on a sorted array to find the presence of an element.
 bool binarySearch(int a[], int l, int r, int x) {
     while(l <= r) {
-        int m = (l + (r - l)) / 2;
+        int m = (l + r) / 2;
 
         if(a[m] == x) {
             return true;
