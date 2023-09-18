5bb5c3d17d3fc7d1f1424867093a2060a434e4a3
Cristian <cristianstevenmottaojeda@gmail.com>
2023-09-17 19:49:06 -0500

    added problems

---
+++ codeforces/B/1352-SameParitySummands.cpp
@@ -0,0 +1,15 @@
+// Made by Cristian Motta (cmottao)
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, k; cin >> n >> k;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t = 1; cin >> t;
+    while(t--) solve();
+}
