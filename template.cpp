4cfecb788c1d6c666f47b2d55bb8f6e268e639aa
cristian <cristianstevenmottaojeda@gmail.com>
2024-04-16 01:55:06 -0500

    modified template

--- template.cpp
+++ template.cpp
@@ -7,11 +7,6 @@ void solve() {
 }
 
 int main() {
-    #if LOCAL
-        freopen("output.txt", "w", stdout);
-        freopen("input.txt", "r", stdin);
-    #endif
-    
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
